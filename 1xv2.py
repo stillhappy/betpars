b5e4f61232ebf3246f7127e761037167c7cc4044
Flomb1k <80835531+Flomb1k@users.noreply.github.com>
2024-02-07 02:08:55 +0300

    Update 1xv2.py

--- 1xv2.py
+++ 1xv2.py
@@ -2,236 +2,188 @@ import requests
 from _datetime import datetime
 
 
-bet_list = []
-def get_final_line(event_result_final, game_id):
-
-    bet_list =[]
+bet_list_final = []
+def get_final_line(event_result_final):
+    global bet_list_final
+    game_name = {'CS 2': 'Counter-Strike','League of Legends': 'Lol'}
     now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    for event in event_result_final['Value']:
-        bet_event = []
-        current_id = event['I']
-        if current_id == game_id:
+    current_time = now.strftime('%d.%m.%Y %H:%M')
 
-            bets = event['E']
-            bet_event.append('line')
-            bet_event.append('1x')
-            game_name = event['L'].split('.')[0]
-            if game_name == 'CS 2':
-                game_name = 'Counter-Strike'
-            if game_name == 'League of Legends':
-                game_name = 'LoL'
-            bet_event.append(game_name)
-            bet_event.append(event['L'].split('.')[1])
-            bet_event.append(event['O1'])
-            bet_event.append(event['O2'])
-            bet_event.append(datetime.fromtimestamp(event['S']).strftime('%d.%m %H:%M'))
-            bet_event.append('Общая')
-            bet_event.append('Исходы')
-            coefs = []
-            for item in bets:
-                if 1 == item['T']:
-                    coef1 = item['C']
-                    coefs.append(coef1)
-                if 2 == item['T']:
-                    coef2 = item['C']
-                    coefs.append(coef2)
-                if 3 == item['T']:
-                    coef3 = item['C']
-                    coefs.append(coef3)
-            bet_event.append(coefs)
-            bet_event.append(current_time)
-            if len(coefs) > 0:
-                bet_list.append(bet_event)
+    for event in event_result_final:
+        item = event['Value']
+        for i in item:
+            sg_value = i.get('SG')
+            if sg_value is not None and len(sg_value) > 0:
+                bet_list = []
+                bet_list.append('line')
+                bet_list.append('1x')
+                game_check = i['SSN']
+                if game_check in game_name:
+                    game_check = game_name[game_check]
+                bet_list.append(game_check)
+                bet_list.append(i['L'].split('.')[1])
+                bet_list.append(i['O1'])
+                bet_list.append(i['O2'])
+                bet_list.append(datetime.fromtimestamp(i['S']).strftime('%d.%m.%Y %H:%M'))
+                bet_list_copy = bet_list.copy()
+                bet_list.append('Общая')
+                bet_list.append('Исходы')
+                kef = []
+                for node in i['E']:
+                    if node['T'] == 1:
+                        kef.append(node['C'])
+                    if node['T'] == 2:
+                        kef.append(node['C'])
+                    if node['T'] == 3:
+                        kef.append(node['C'])
+                bet_list.append(kef)
+                bet_list.append(current_time)
+                if len(bet_list[9]) > 1:
+                    bet_list_final.append(bet_list)
+        for i in item:
+                try:
+                    bets = i['SG']
+                except KeyError:
+                    continue
+                kef = []
+                for node in bets:
+                    for item in node['E']:
+                        if item['T'] == 1:
+                            kef.append(item['C'])
+                        if item['T'] == 3:
+                            kef.append(item['C'])
+                if len(kef) > 0:
+                    bet_map = bet_list_copy.copy()
+                    kef_map = [kef[0],kef[1]]
+                    bet_map.append('1-я карта')
+                    bet_map.append('Исходы')
+                    bet_map.append(kef_map)
+                    bet_map.append(current_time)
+                    if len(bet_map[9]) > 1:
+                        bet_list_final.append(bet_map)
+                if len(kef) > 2:
+                    bet_map = bet_list_copy.copy()
+                    kef_map = [kef[2],kef[3]]
+                    bet_map.append('2-я карта')
+                    bet_map.append('Исходы')
+                    bet_map.append(kef_map)
+                    bet_map.append(current_time)
+                    if len(bet_map[9]) > 1:
+                        bet_list_final.append(bet_map)
+                if len(kef) > 4:
+                    bet_map = bet_list_copy.copy()
+                    kef_map = [kef[4],kef[5]]
+                    bet_map.append('3-я карта')
+                    bet_map.append('Исходы')
+                    bet_map.append(kef_map)
+                    bet_map.append(current_time)
+                    if len(bet_map[9]) > 1:
+                        bet_list_final.append(bet_map)
+                kef = []
+                for node in bets:
+                    for item in node['E']:
+                        if item['T'] == 7:
+                            kef.append(item['C'])
+                            kef.append(item['P'])
+                        if item['T'] == 8:
+                            kef.append(item['C'])
 
-        # Получение списков с форами
-        if current_id == game_id:
-            bet_fora_d = {}
-            bet_fora_d['Название матча'] = event['L']
-            bet_fora_d['Team 1'] = event['O1']
-            bet_fora_d['Team 2'] = event['O2']
-            bet_fora_d['Время'] = event['S']
-            try:
-                bets = event['SG']
-            except KeyError:
-                continue
-            for item in bets:
-                bet = item['PN']
-                map = ''
-                coefs = []
-                for node in item['E']:
-                    table_cell = node['T']
-                    map = item['PN']
-                    bet_fora_d['Карта'] = map
-                    if 7 == table_cell:
-                        fora = node['P']
-                        fora = str(fora)
-                        if fora.startswith('-'):
-                            bet_fora_d['Исход'] = 'Фора 1 (' + str(fora) + ')'
-                        else:
-                            fora ='+' + fora
-                            bet_fora_d['Исход'] = 'Фора 1 (' + str(fora) + ')'
-                        coef1 = node['C']
+                if len(kef) > 0:
+                    bet_fora = bet_list_copy.copy()
+                    kef_fora = [kef[0],kef[2]]
+                    bet_fora.append('1-я карта')
+                    bet_fora.append('Фора 1 (' + str(kef[1]) + ')')
+                    bet_fora.append(kef_fora)
+                    bet_fora.append(current_time)
+                    if len(bet_fora[9]) > 1:
+                        bet_list_final.append(bet_fora)
+                if len(kef) > 3:
+                    bet_fora = bet_list_copy.copy()
+                    kef_fora = [kef[3],kef[5]]
+                    bet_fora.append('2-я карта')
+                    bet_fora.append('Фора 1 (' + str(kef[4]) + ')')
+                    bet_fora.append(kef_fora)
+                    bet_fora.append(current_time)
+                    if len(bet_fora[9]) > 1:
+                        bet_list_final.append(bet_fora)
+                if len(kef) > 6:
+                    bet_fora = bet_list_copy.copy()
+                    kef_fora = [kef[6],kef[8]]
+                    bet_fora.append('3-я карта')
+                    bet_fora.append('Фора 1 (' + str(kef[7]) + ')')
+                    bet_fora.append(kef_fora)
+                    bet_fora.append(current_time)
+                    if len(bet_fora[9]) > 1:
+                        bet_list_final.append(bet_fora)
 
-                        coefs.append(coef1)
-                    if 8 == table_cell:
-                        fora = node['P']
-                        coef2 = node['C']
-                        coefs.append(coef2)
-                bet_fora = []
-                bet_fora.append('line')
-                bet_fora.append('1x')
-                game_name = event['L'].split('.')[0]
-                if game_name == 'CS 2':
-                    game_name = 'Counter-Strike'
-                if game_name == 'League of Legends':
-                    game_name = 'LoL'
-                bet_fora.append(game_name)
-                bet_fora.append(event['L'].split('.')[1])
-                bet_fora.append(event['O1'])
-                bet_fora.append(event['O2'])
-                bet_fora.append(datetime.fromtimestamp(event['S']).strftime('%d.%m %H:%M'))
-                bet_fora.append(map)
-                if 'Исход' in bet_fora_d:
-                    bet_fora.append(bet_fora_d['Исход'])
+                kef = []
+                for node in bets:
+                    for item in node['E']:
+                        if item['T'] == 9:
+                            kef.append(item['C'])
+                            kef.append(item['P'])
+                        if item['T'] == 10:
+                            kef.append(item['C'])
 
-                bet_fora.append(coefs)
-                bet_fora.append(current_time)
-
-                if len(coefs) > 0:
-                    bet_list.append(bet_fora)
-        # Получение списков с тоталами
-        if current_id == game_id:
-            bet_total_d = {}
-            bet_total_d['Название матча'] = event['L']
-            bet_total_d['Team 1'] = event['O1']
-            bet_total_d['Team 2'] = event['O2']
-            bet_total_d['Время'] = event['S']
-            try:
-                bets = event['SG']
-            except KeyError:
-                continue
-            for item in bets:
-                bet = item['PN']
-                map = ''
-                coefs = []
-                for node in item['E']:
-                    table_cell = node['T']
-                    map = item['PN']
-                    bet_total_d['Карта'] = map
-                    if 9 == table_cell:
-                        total = node['P']
-                        coef1 = node['C']
-                        bet_total_d['Исход'] = 'Тотал Б (' + str(total) +')'
-                        coefs.append(coef1)
-                    if 10 == table_cell:
-                        coef2 = node['C']
-                        coefs.append(coef2)
-                bet_total = []
-                bet_total.append('line')
-                bet_total.append('1x')
-                game_name = event['L'].split('.')[0]
-                if game_name == 'CS 2':
-                    game_name = 'Counter-Strike'
-                if game_name == 'League of Legends':
-                    game_name = 'LoL'
-                bet_total.append(game_name)
-                bet_total.append(event['L'].split('.')[1])
-                bet_total.append(event['O1'])
-                bet_total.append(event['O2'])
-                bet_total.append(datetime.fromtimestamp(event['S']).strftime('%d.%m %H:%M'))
-                bet_total.append(map)
-                if 'Исход' in bet_total_d:
-                    bet_total.append(bet_total_d['Исход'])
-
-                bet_total.append(coefs)
-                bet_total.append(current_time)
-
-                if len(coefs) > 0:
-                    bet_list.append(bet_total)
-        # Получение списков с исходами на карты
-        if current_id == game_id:
-            bet_map_d = {}
-            bet_map_d['Название матча'] = event['L']
-            bet_map_d['Team 1'] = event['O1']
-            bet_map_d['Team 2'] = event['O2']
-            bet_map_d['Время'] = event['S']
-            try:
-                bets = event['SG']
-            except KeyError:
-                continue
-            for item in bets:
-                bet = item['PN']
-                map = ''
-                coefs = []
-                for node in item['E']:
-                    table_cell = node['T']
-                    map = item['PN']
-                    bet_map_d['Карта'] = map
-                    if 1 == table_cell:
-                        coef1 = node['C']
-                        coefs.append(coef1)
-                    if 3 == table_cell:
-                        coef2 = node['C']
-                        coefs.append(coef2)
-                bet_map = []
-                bet_map.append('line')
-                bet_map.append('1x')
-                game_name = event['L'].split('.')[0]
-                if game_name == 'CS 2':
-                    game_name = 'Counter-Strike'
-                if game_name == 'League of Legends':
-                    game_name = 'LoL'
-                bet_map.append(game_name)
-                bet_map.append(event['L'].split('.')[1])
-                bet_map.append(event['O1'])
-                bet_map.append(event['O2'])
-                bet_map.append(datetime.fromtimestamp(event['S']).strftime('%d.%m %H:%M'))
-                bet_map.append(map)
-                bet_map.append('Исходы')
-                bet_map.append(coefs)
-                bet_map.append(current_time)
-
-                if len(coefs) > 0:
-                    bet_list.append(bet_map)
-
-
-    temp_list = []
-    for i in bet_list:
-        if i not in temp_list:
-            temp_list.append((i))
-    bet_list = temp_list
-
-    print(*bet_list, sep='\n')
-    return bet_list
+                if len(kef) > 0:
+                    bet_total = bet_list_copy.copy()
+                    kef_total = [kef[0], kef[2]]
+                    bet_total.append('1-я карта')
+                    bet_total.append('Тотал Б (' + str(kef[1]) + ')')
+                    bet_total.append(kef_total)
+                    bet_total.append(current_time)
+                    if len(bet_total[9]) > 1:
+                        bet_list_final.append(bet_total)
+                if len(kef) > 3:
+                    bet_total = bet_list_copy.copy()
+                    kef_total = [kef[3], kef[5]]
+                    bet_total.append('2-я карта')
+                    bet_total.append('Тотал Б (' + str(kef[4]) + ')')
+                    bet_total.append(kef_total)
+                    bet_total.append(current_time)
+                    if len(bet_total[9]) > 1:
+                        bet_list_final.append(bet_total)
+                if len(kef) > 6:
+                    bet_total = bet_list_copy.copy()
+                    kef_total = [kef[6], kef[8]]
+                    bet_total.append('3-я карта')
+                    bet_total.append('Тотал Б (' + str(kef[7]) + ')')
+                    bet_total.append(kef_total)
+                    bet_total.append(current_time)
+                    if len(bet_total[9]) > 1:
+                        bet_list_final.append(bet_total)
+    return bet_list_final
 
 def get_1x_line_value(event_result):
+    event_result_final = []
     session = requests.Session()
-    for item in event_result['Value']:
-        game_id = item['I']
-        champs = item['LI']
 
-        params = {
-            'sports': '40',
-            'champs': champs,
-            'count': '50',
-            'tf': '2200000',
-            'tz': '3',
-            'antisports': '188',
-            'mode': '4',
-            'subGames': game_id,
-            'country': '1',
-            'partner': '51',
-            'getEmpty': 'true',
-            'gr': '44',
-        }
+    for event in event_result:
+        for item in event['Value']:
+            champs = (item['LI'])
+            game_id =(item['I'])
+            params = {
+                'sports': '40',
+                'champs': champs,
+                'count': '50',
+                'tf': '2200000',
+                'tz': '3',
+                'antisports': '188',
+                'mode': '4',
+                'subGames': game_id,
+                'country': '1',
+                'partner': '51',
+                'getEmpty': 'true',
+                'gr': '44',
+            }
 
-        response = session.get('https://1xstavka.ru/LineFeed/Get1x2_VZip', params=params)
-        event_result_final = response.json()
-        get_final_line(event_result_final, game_id)
-
+            response = session.get('https://1xstavka.ru/LineFeed/Get1x2_VZip', params=params)
+            event_result_final.append(response.json())
+    bet_list_final = get_final_line(event_result_final)
+    return bet_list_final
 
 def get_1x_line(champ):
+    event_result = []
     session = requests.Session()
     for i in champ:
         params = {
@@ -250,10 +202,11 @@ def get_1x_line(champ):
         }
 
         response = session.get('https://1xstavka.ru/LineFeed/Get1x2_VZip', params=params)
-        event_result = response.json()
+        event_result.append(response.json())
 
-        get_1x_line_value(event_result)
 
+    bet_list_final = get_1x_line_value(event_result)
+    return bet_list_final
 
 def main():
     session = requests.Session()
@@ -274,8 +227,9 @@ def main():
         if 'League of Legends' == ci['SSN'] or 'Dota 2' == ci['SSN'] or 'CS 2' == ci['SSN'] or 'Valorant' == ci['SSN']:
             champ_id.append(ci['LI'])
 
+    bet_list_final = get_1x_line(champ_id)
 
-    get_1x_line(champ_id)
+    print(*bet_list_final, sep = '\n')
 
 
 
